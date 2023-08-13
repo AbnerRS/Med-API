167e53d33d6a6ba8f18ddfc6ca1ab1bcf541c3c2
AbnerRS <abnersesisenai@gmail.com>
2023-08-12 23:55:33 -0300

    MED_API - V4 - Interfaces Repository.

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -22,11 +22,14 @@ import med.voll.api.endereco.Endereco;
 @EqualsAndHashCode(of = "id")
 public class Medico {
 
+  
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String crm;
+
+      
 
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
@@ -34,4 +37,13 @@ public class Medico {
     @Embedded
     private Endereco endereco;
     
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+        
+    }
 }
