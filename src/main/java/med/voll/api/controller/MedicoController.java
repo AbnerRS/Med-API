a7fe985c22bc3df1a45495a30642249023236b45
AbnerRS <abnersesisenai@gmail.com>
2023-08-20 17:15:07 -0300

    V15 - Data breakdown.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -69,4 +69,12 @@ public class MedicoController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+
+        var medico = repository.getReferenceById(id);
+      
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+    
 }
