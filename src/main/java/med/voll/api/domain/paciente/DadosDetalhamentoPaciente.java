26bd5fbe8bc9c9d21ba82c2741b12884565de835
AbnerRS <abnersesisenai@gmail.com>
2023-08-21 10:58:58 -0300

    MED_API - V18 - organizing structure and 404 error.

---
+++ src/main/java/med/voll/api/domain/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.paciente;
+import med.voll.api.domain.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) { 
+    public DadosDetalhamentoPaciente(Paciente paciente) { 
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco()); 
+    }
+} 
---
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,16 @@
+package med.voll.api.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+}
