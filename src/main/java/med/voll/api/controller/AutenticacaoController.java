8a85291a20e44d162492225f1fb710c1608c68e2
AbnerRS <abnersesisenai@gmail.com>
2023-08-22 18:44:02 -0300

    MED_API - V27 - Generating Tokens JWT.

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.TokenService;
 
 @RestController
 @RequestMapping("/login")
@@ -18,12 +20,15 @@ public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
-    
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
-        
-        return ResponseEntity.ok().build();
-    }
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+    var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+    var authentication = manager.authenticate(token);
+
+    return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+}
 }
