5d04c0eb179f4f35e274c63cf6d409eeb117bed8
AbnerRS <abnersesisenai@gmail.com>
2023-08-22 18:57:43 -0300

    MED_API - V28 - Token Gerneration tweaks.

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -10,14 +10,19 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario) { 
         try {
-            var algoritmo = Algorithm.HMAC256("12345678");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                 .withIssuer("API Voll.med")
                 .withSubject(usuario.getLogin())
