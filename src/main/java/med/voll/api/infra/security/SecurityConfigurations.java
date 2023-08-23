9e483f235d7b3bb00a1ad6c9b25eddd1bd04eaee
AbnerRS <abnersesisenai@gmail.com>
2023-08-23 09:58:38 -0300

    MED_API - V34 - removing deprecated code.

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -22,15 +22,17 @@ public class SecurityConfigurations{
     private SecurityFilter securityFilter;
     
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().authorizeHttpRequests()
-            .requestMatchers(HttpMethod.POST, "/login").permitAll()
-            .anyRequest().authenticated()
-            .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(csrf -> csrf.disable())
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(req -> {
+                req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                req.anyRequest().authenticated();
+            })
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
-    }
+}
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
