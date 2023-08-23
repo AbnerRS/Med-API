6d9b485df7661d4976a1ea2840018a289f291f9d
AbnerRS <abnersesisenai@gmail.com>
2023-08-23 12:17:57 -0300

    MED_API - V39 - Random doctor choice.

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -15,7 +15,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long>{
     @Query("""
             select m from Medico m 
             where
-            m.ativo = 1
+            m.ativo = true
             and
             m.especialidade = :especialidade
             and
