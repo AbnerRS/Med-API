c841547cef67a9a0366b178799cbade4432a0f1f
AbnerRS <abnersesisenai@gmail.com>
2023-08-23 12:04:08 -0300

    MED_API - V38 - Integrity Validation.

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -3,6 +3,8 @@ package med.voll.api.domain.consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 
@@ -21,10 +23,23 @@ public class AgendaDeConsultas {
     
     public void agendar(DadosAgendamentoConsulta dados){
 
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidationException("ID do paciente informado não existe!");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidationException("ID do médico informado não existe!");
+        }
+
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         
         consultaRepository.save(consulta);
+    }
+
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        return null;
     }
 }
