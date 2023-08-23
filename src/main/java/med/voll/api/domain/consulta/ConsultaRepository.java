57f336a629f5e38df87c3f93cf02279716c88694
AbnerRS <abnersesisenai@gmail.com>
2023-08-23 11:11:57 -0300

    MED_API - V36 - Appointment Registration.

---
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.consulta;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+}
