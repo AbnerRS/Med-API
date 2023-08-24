38a9d341d9148947f2c1723caf4e63e0dd281525
AbnerRS <abnersesisenai@gmail.com>
2023-08-24 11:23:17 -0300

    MED_API - V48 - Test Database Configuration.

---
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class MedicoRepositoryTest {
+
+    @Test
+    void testEscolherMedicoAleatorioLivreNaData() {
+
+    }
+}
